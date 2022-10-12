cbc885157b6ed2e1305ab0bdd39d451c3901d2a8
Eh.Emon <eh.emon3059@gmail.com>
2022-10-12 17:31:32 +0600

    blogs card design complete

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,10 +1,39 @@
-
+import './Blogs.css'
 const Blogs = () => {
 
 
     return (
         <div>
             <h1>THis is blogs</h1>
+            <div className="main-container">
+                <div className="child-container">
+                    <div className="heading">
+                        <h1>This is Heading</h1>
+                    </div>
+                    
+                    <div className="question-answer">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta id, molestiae eos accusantium officiis dolor quo nulla iusto aperiam sequi cupiditate dicta labore distinctio corporis, facere officia ratione accusamus sapiente voluptatem modi unde voluptate. Quo nisi esse doloremque aperiam! Officia voluptas nesciunt ea delectus exercitationem labore pariatur, perferendis hic harum?</p>
+                    </div>
+                </div>
+                <div className="child-container">
+                    <div className="heading">
+                        <h1>This is Heading</h1>
+                    </div>
+                    
+                    <div className="question-answer">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta id, molestiae eos accusantium officiis dolor quo nulla iusto aperiam sequi cupiditate dicta labore distinctio corporis, facere officia ratione accusamus sapiente voluptatem modi unde voluptate. Quo nisi esse doloremque aperiam! Officia voluptas nesciunt ea delectus exercitationem labore pariatur, perferendis hic harum?</p>
+                    </div>
+                </div>
+                <div className="child-container">
+                    <div className="heading">
+                        <h1>This is Heading</h1>
+                    </div>
+                    
+                    <div className="question-answer">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta id, molestiae eos accusantium officiis dolor quo nulla iusto aperiam sequi cupiditate dicta labore distinctio corporis, facere officia ratione accusamus sapiente voluptatem modi unde voluptate. Quo nisi esse doloremque aperiam! Officia voluptas nesciunt ea delectus exercitationem labore pariatur, perferendis hic harum?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
